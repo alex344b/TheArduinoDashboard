2b223332eb6b08740f6a43e81a1f528c07453c06
alex344b <alex344b@edu.mercantec.dk>
2026-01-08 14:42:30 +0100

    initial commit

---
+++ TheArduinoDashboard/Components/Pages/CatFactDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace TheArduinoDashboard.Components.Pages;
+
+public class CatFactDTO
+{
+    public DateTime dateAdded { get; set; }
+    public string fact { get; set; }
+    public int likes { get; set; }
+    public int dislikes { get; set; }
+}
+
+public class Pages
+{
+    public List<CatFactDTO> data { get; set; }
+    public int page { get; set; }
+    public int pagesize { get; set; }
+    public int toatlCount { get; set; }
+    public int totalPages { get; set; }
+}
